9681fa93c05523ab9f8aab8c3764b8f713ca6a1b
Rezi <reziyemu.sulaiman@gmail.com>
2024-07-29 21:25:35 -0400

    assignment3

--- src/app.js
+++ src/app.js
@@ -1,141 +1,52 @@
 // src/app.js
 
 import { Auth, getUser } from './auth';
-
 import { getUserFragments, getUserFragmentsExpanded } from './api';
 
 async function init() {
   const userSection = document.querySelector('#user');
   const loginBtn = document.querySelector('#login');
   const logoutBtn = document.querySelector('#logout');
-  
   const postTpBtn = document.querySelector('#textPlainBtn');
   const postMdBtn = document.querySelector('#textMdBtn');
   const postHtmlBtn = document.querySelector('#textHtmlBtn');
   const postJsonBtn = document.querySelector('#applicationJsonBtn');
-  
+  const putBtn = document.querySelector('#putBtn');
+  const delBtn = document.querySelector('#delBtn');
   const expandBtn = document.querySelector('#expand');
   const expandBtn2 = document.querySelector('#expand2');
   
   const user = await getUser();
 
   // Wire up event handlers to deal with login and logout.
-  loginBtn.onclick = () => {
-    // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
-    // https://docs.amplify.aws/lib/auth/advanced/q/platform/js/#identity-pool-federation
-    Auth.federatedSignIn();
-  };
-  logoutBtn.onclick = () => {
-    // Sign-out of the Amazon Cognito Hosted UI (requires redirects), see:
-    // https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js/#sign-out
-    Auth.signOut();
-  };
+  loginBtn.onclick = () => Auth.federatedSignIn();
+  logoutBtn.onclick = () => Auth.signOut();
 
-  // See if we're signed in (i.e., we'll have a `user` object)
-  //const user = await getUser();
   if (!user) {
-    // Disable the Logout button
     logoutBtn.disabled = true;
     return;
   }
 
-  // Log the user info for debugging purposes
   console.log({ user });
-
-  // Update the UI to welcome the user
   userSection.hidden = false;
-
-  // Show the user's username
   userSection.querySelector('.username').innerText = user.username;
-
-  // Disable the Login button
   loginBtn.disabled = true;
-  
-  // Do an authenticated request to the fragments API server and log the result
+
   getUserFragments(user);
   getUserFragmentsExpanded(user);
 
-
   const apiUrl = process.env.API_URL || 'http://localhost:8080';
-  // Post button
-  postTpBtn.onclick = async() => {
-    console.log('POST fragments data...');
-    console.log('Posting: ' + document.querySelector('#textfield').value);
-    try {
-      const res = await fetch(`${apiUrl}/v1/fragments`, {
-        method: "POST",
-        body: document.querySelector('#textfield').value,
-        // Generate headers with the proper Authorization bearer token to pass
-        headers: {
-          Authorization: user.authorizationHeaders().Authorization,
-          "Content-Type" : "text/plain",
-        }
-      });
-      if (!res.ok) {
-        throw new Error(`${res.status} ${res.statusText}`);
-      }
-      const data = await res.json();
-      console.log('Posted user fragments data', { data });
-    } catch (err) {
-      console.error('Unable to POST to /v1/fragment', { err });
-    }
-  }
-  postMdBtn.onclick = async() => {
-    console.log('POST fragments data...');
-    console.log('Posting: ' + document.querySelector('#textfield').value);
-    try {
-      const res = await fetch(`${apiUrl}/v1/fragments`, {
-        method: "POST",
-        body: document.querySelector('#textfield').value,
-        // Generate headers with the proper Authorization bearer token to pass
-        headers: {
-          Authorization: user.authorizationHeaders().Authorization,
-          "Content-Type" : "text/markdown",
-        }
-      });
-      if (!res.ok) {
-        throw new Error(`${res.status} ${res.statusText}`);
-      }
-      const data = await res.json();
-      console.log('Posted user fragments data', { data });
-    } catch (err) {
-      console.error('Unable to POST to /v1/fragment', { err });
-    }
-  }
-  postHtmlBtn.onclick = async() => {
-    console.log('POST fragments data...');
-    console.log('Posting: ' + document.querySelector('#textfield').value);
-    try {
-      const res = await fetch(`${apiUrl}/v1/fragments`, {
-        method: "POST",
-        body: document.querySelector('#textfield').value,
-        // Generate headers with the proper Authorization bearer token to pass
-        headers: {
-          Authorization: user.authorizationHeaders().Authorization,
-          "Content-Type" : "text/html",
-        }
-      });
-      if (!res.ok) {
-        throw new Error(`${res.status} ${res.statusText}`);
-      }
-      const data = await res.json();
-      console.log('Posted user fragments data', { data });
-    } catch (err) {
-      console.error('Unable to POST to /v1/fragment', { err });
-    }
-  }
 
-  postJsonBtn.onclick = async() => {
+  const postData = async (contentType, body) => {
     console.log('POST fragments data...');
-    console.log('Posting: ' + document.querySelector('#textfield').value);
+    console.log('Posting: ', body);
     try {
       const res = await fetch(`${apiUrl}/v1/fragments`, {
         method: "POST",
-        body: document.querySelector('#textfield').value,
-        // Generate headers with the proper Authorization bearer token to pass
+        body: body,
         headers: {
           Authorization: user.authorizationHeaders().Authorization,
-          "Content-Type" : "application/json",
+          "Content-Type": contentType,
         }
       });
       if (!res.ok) {
@@ -146,74 +57,76 @@ async function init() {
     } catch (err) {
       console.error('Unable to POST to /v1/fragment', { err });
     }
-  }
-  
-  
-  // Display fragments
-  expandBtn.onclick = async() => {
+  };
+
+  postTpBtn.onclick = () => postData("text/plain", document.querySelector('#textfield').value);
+  postMdBtn.onclick = () => postData("text/markdown", document.querySelector('#textfield').value);
+  postHtmlBtn.onclick = () => postData("text/html", document.querySelector('#textfield').value);
+  postJsonBtn.onclick = () => postData("application/json", document.querySelector('#textfield').value);
+
+  expandBtn.onclick = async () => {
     console.log('Requesting user fragments data...');
     try {
-      const res = await fetch(`${apiUrl}/v1/fragments?expand=1`, {
-        // Generate headers with the proper Authorization bearer token to pass
-        headers: user.authorizationHeaders(),
-      });
+      const res = await fetch(`${apiUrl}/v1/fragments?expand=1`, { headers: user.authorizationHeaders() });
       if (!res.ok) {
         throw new Error(`${res.status} ${res.statusText}`);
       }
       const data = await res.json();
       console.log('Got user fragments data', { data });
-      // Loop through res fragments and fetch
       document.getElementById("displayFragments").innerHTML = "";
       for (const fragment of data.fragments) {
         try {
-          const res = await fetch(`${apiUrl}/v1/fragments/${fragment.id}`, {
-            // Generate headers with the proper Authorization bearer token to pass
-            headers: user.authorizationHeaders(),
-          });
+          const res = await fetch(`${apiUrl}/v1/fragments/${fragment.id}`, { headers: user.authorizationHeaders() });
           if (!res.ok) {
             throw new Error(`${res.status} ${res.statusText}`);
           }
-          // Populate with text fragment
           if (fragment.type.startsWith("text") || fragment.type.startsWith("application")) {
             const data = await res.text();
             document.getElementById("displayFragments").appendChild(document.createElement("hr"));
             document.getElementById("displayFragments").appendChild(document.createTextNode("Fragment id: " + fragment.id));
             document.getElementById("displayFragments").appendChild(document.createElement("br"));
             document.getElementById("displayFragments").appendChild(document.createTextNode(data));
-          } 
-         } catch (err) {
+          } else if (fragment.type.startsWith("image")) {
+            const data = await res.arrayBuffer();
+            const rawData = Buffer.from(data);
+            const imageData = new Blob([rawData], {type: fragment.type});
+            const image = new Image();
+            const reader = new FileReader();
+            reader.readAsDataURL(imageData);
+            reader.addEventListener("load", function () {
+              image.src = reader.result;
+              image.alt = fragment.id;
+            });
+            document.getElementById("displayFragments").appendChild(document.createElement("hr"));
+            document.getElementById("displayFragments").appendChild(document.createTextNode("Fragment id: " + fragment.id));
+            document.getElementById("displayFragments").appendChild(document.createElement("br"));
+            document.getElementById("displayFragments").appendChild(image);
+          }
+        } catch (err) {
           console.error("Unable to call GET /v1/fragments/:id", { err });
-         }
+        }
       }
     } catch (err) {
       console.error('Unable to call GET /v1/fragment', { err });
     }
-  }
+  };
 
-  expandBtn2.onclick = async() => {
+  expandBtn2.onclick = async () => {
     console.log('Requesting user fragments data...');
     try {
-      const res = await fetch(`${apiUrl}/v1/fragments?expand=1`, {
-        // Generate headers with the proper Authorization bearer token to pass
-        headers: user.authorizationHeaders(),
-      });
+      const res = await fetch(`${apiUrl}/v1/fragments?expand=1`, { headers: user.authorizationHeaders() });
       if (!res.ok) {
         throw new Error(`${res.status} ${res.statusText}`);
       }
       const data = await res.json();
       console.log('Got user fragments metadata', { data });
-      // Loop through res fragments and fetch
       document.getElementById("displayFragments").innerHTML = "";
       for (const fragment of data.fragments) {
         try {
-          const res = await fetch(`${apiUrl}/v1/fragments/${fragment.id}/info`, {
-            // Generate headers with the proper Authorization bearer token to pass
-            headers: user.authorizationHeaders(),
-          });
+          const res = await fetch(`${apiUrl}/v1/fragments/${fragment.id}/info`, { headers: user.authorizationHeaders() });
           if (!res.ok) {
             throw new Error(`${res.status} ${res.statusText}`);
           }
-          // Populate with fragment metadata
           const data = await res.text();
           document.getElementById("displayFragments").appendChild(document.createElement("hr"));
           document.getElementById("displayFragments").appendChild(document.createTextNode("Fragment id: " + fragment.id));
@@ -226,8 +139,92 @@ async function init() {
     } catch (err) {
       console.error('Unable to call GET /v1/fragment', { err });
     }
-  }
+  };
 
+  const imgUpload = async (event) => {
+    console.log('POST fragments data...');
+    console.log(event.target.files);
+    for (const file of event.target.files) {
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+      reader.onload = async (item) => {
+        const res = await fetch(`${apiUrl}/v1/fragments`, {
+          method: "POST",
+          body: item.target.result,
+          headers: {
+            Authorization: user.authorizationHeaders().Authorization,
+            "Content-Type": file.type,
+          },
+        });
+        const data = await res.json();
+        console.log('Posted user fragments data', { data });
+      };
+    }
+  };
+  document.querySelector("#imageFile").addEventListener("change", imgUpload);
+
+  // app.js
+
+  putBtn.onclick = async () => {
+    console.log('PUT fragments data...');
+    const modId = document.querySelector('#fragId').value;
+    const newValue = document.querySelector('#textfield2').value;
+    const contentType = document.querySelector('#contentType').value; // Assuming there's an input for content type
+    console.log('PUT: ', modId, ' with value: ', newValue, ' and content type: ', contentType);
+    
+    let body;
+    
+    // Handle different content types
+    // if (contentType === "application/json") {
+    //   body = JSON.stringify(newValue); // Ensure JSON is stringified
+    // } else {
+      body = newValue; // For text types, use the string directly
+    //}
+    
+    try {
+      const res = await fetch(`${apiUrl}/v1/fragments/${modId}`, {
+        method: "PUT",
+        body: body,
+        headers: {
+          Authorization: user.authorizationHeaders().Authorization,
+          "Content-Type": contentType, // Use the dynamic content type
+        }
+      });
+      if (!res.ok) {
+        const errorResponse = await res.json();
+        throw new Error(errorResponse.error.message);
+      }
+      const data = await res.json();
+      console.log('PUT user fragments data', { data });
+    } catch (err) {
+      console.error('Unable to PUT to /v1/fragment', { err });
+    }
+  };
+
+
+
+
+
+  delBtn.onclick = async () => {
+    console.log('DELETE fragment');
+    const modId = document.querySelector('#fragId').value;
+    console.log('DELETE fragment: ', modId);
+    try {
+      const res = await fetch(`${apiUrl}/v1/fragments/${modId}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: user.authorizationHeaders().Authorization,
+        }
+      });
+      if (!res.ok) {
+        throw new Error(`${res.status} ${res.statusText}`);
+      }
+      const data = await res.json();
+      console.log('DELETE user fragments data', { data });
+    } catch (err) {
+      console.error('Unable to DELETE to /v1/fragment', { err });
+    }
+  };
 }
 
 // Wait for the DOM to be ready, then start the app
